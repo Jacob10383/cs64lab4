00cf34f86cc967700051970775e7566a0b2c3797
Jacob10383 <36573259+Jacob10383@users.noreply.github.com>
2022-10-24 22:05:38 -0700

    Update min.asm

--- min.asm
+++ min.asm
@@ -5,13 +5,21 @@
 
         # TODO: Complete these declarations / initializations
 
-        .asciiz "Enter the next number:\n"
-        .asciiz "Minimum: "
-    .asciiz "\n"
+        str1: .asciiz "Enter the next number:\n"
+        str2: .asciiz "Minimum: "
+    str3: .asciiz "\n"
 
 #Text Area (i.e. instructions)
 .text
-
+la $a0,str1 #Load and print string asking for num
+li $v0,4
+syscall
+la $a0,str2 #Load and print nextline
+li $v0,4
+syscall
+li $v0, 5   #read in int
+syscall
+move $t0, $v0   #put int in t0
 main:
 
         # TODO: Write your code here
