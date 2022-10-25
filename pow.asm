a60ae0e516f5dcce12da42277ce9274fb93e1d53
Jacob10383 <36573259+Jacob10383@users.noreply.github.com>
2022-10-25 12:16:22 -0700

    Update pow.asm

--- pow.asm
+++ pow.asm
@@ -15,12 +15,29 @@
 # Assembly (NON-RECURSIVE) code version of pow(x,n):
 #
 .data
+	str1: .asciiz "Enter a number x:\n"
+	str2: .asciiz "Enter the exponent n:\n"
+	str3: .asciiz "Power(x,n) is:\n" << pow << "\n"
+
 
 	# TODO: Write your initializations here
 
 #Text Area (i.e. instructions)
 .text
 main:
+la $a0,str1 #Load and print string asking for num
+li $v0,4
+syscall
+li $v0, 5   #read in int
+syscall
+move $t0, $v0   #put int in t0
+
+la $a0,str1 #Load and print string asking for num
+li $v0,4
+syscall
+li $v0, 5   #read in int
+syscall
+move $t1, $v0   #put int in t0
 	
 	# TODO: Write your code here
 
